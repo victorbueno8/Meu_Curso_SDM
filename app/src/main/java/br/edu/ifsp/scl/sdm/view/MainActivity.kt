2026a4259c104f445dda8e3c5da16983ee5997b7
Victor Bueno <vihto8@gmail.com>
2019-09-18 09:19:30 -0300

    Add Context Menu

--- app/src/main/java/br/edu/ifsp/scl/sdm/view/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/view/MainActivity.kt
@@ -2,13 +2,17 @@ package br.edu.ifsp.scl.sdm.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.View
+import android.widget.AdapterView
+import androidx.appcompat.app.AlertDialog
 import br.edu.ifsp.scl.sdm.R
 import br.edu.ifsp.scl.sdm.adapter.CursoController
 import br.edu.ifsp.scl.sdm.adapter.DisciplinasAdapter
 import br.edu.ifsp.scl.sdm.model.Disciplina
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
     lateinit var listaDisciplinas: MutableList<Disciplina>
     lateinit var disciplinasAdapter: DisciplinasAdapter
     lateinit var cursoController: CursoController
@@ -32,6 +36,12 @@ class MainActivity : AppCompatActivity() {
 
         // Solicitando todas as disciplinas para o controller
         cursoController.buscaTodas()
+
+        // Registra o ListView para conter menu decontexto
+        registerForContextMenu(disciplinasLv)
+
+        //Setando listener de clique nos itens
+        disciplinasLv.setOnItemClickListener(this)
     }
 
     private fun insereDisciplinasFalsas() {
@@ -48,6 +58,26 @@ class MainActivity : AppCompatActivity() {
         listaDisciplinas.addAll(listaAtualizada)
         // Notifica adapter das mudancas na sua fonte de dados
         disciplinasAdapter.notifyDataSetChanged()
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        menuInflater.inflate(R.menu.menu_contexto, menu)
+    }
+
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val disciplina = listaDisciplinas[position]
+
+        // Criando AlertDialog pra mostrar a ementa da disciplina
+        val ementaDialog = with(AlertDialog.Builder(this)) {
+            setTitle(disciplina.codigo)
+            setMessage("${disciplina.nome} :\n ${disciplina.ementa}")
+            create()
+        }
+        ementaDialog.show()
     }
 
 }
